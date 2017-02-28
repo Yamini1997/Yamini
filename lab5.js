5eca796a3fb05692bd705945288c71f7b183697e
Yamini1997 <yamini2497@gmail.com>
2017-02-28 15:52:55 +0530

    Update lab5.js

--- lab5.js
+++ lab5.js
@@ -1,5 +1,32 @@
-var points = [40, 100, 1, 5, 25, 10];
-function myFunction() 
+var a = [2,5,3,1,6,8];
+var x = 4;
+var result;
+function c1(at, x)
 {
-    points.sort(function(a, b){return a-b});
+var i;
+var j;
+var r;
+var temp;
+for(i=0; i<(at.length-1); i++)
+{
+	for(j=1; j<at.length; j++)
+	{
+		if(at[i]>at[j])
+		{
+			temp=at[j];
+			at[j]=at[i];
+			at[i]=temp;
+		}
+	}
 }
+
+console.log("Sorted array:");
+for(var t=0;t<at.length; i++)
+{
+	console.log(at[t]);
+}
+
+result= at[x-1];
+console.log("The answer is:" +result);
+}
+c1(a,4);
