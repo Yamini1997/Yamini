e2a9c40cf06db4c3a211db6d501db869fac3ef92
Yamini1997 <yamini2497@gmail.com>
2017-03-07 15:22:27 +0530

    Update script.js

--- example/js/script.js
+++ example/js/script.js
@@ -1,1 +1,32 @@
+var cnt = 0;
+function sayHello () {
+  var name =
+   document.getElementById("name").value;
+   var message = "<h2>Hello " + name + "!</h2>";
+  
+  document
+    .getElementById("content")
+    .innerHTML = message;
+
+  if (name === "student") {
+    var title = 
+      document
+        .querySelector("#title")
+        .textContent;
+    if (cnt === 0) {
+    title += " & Lovin' it!";
+    cnt++;
+      document
+        .querySelector("h1")
+        .textContent = title;
+  }
+    else{
+      cnt = 0;
+      var title =
+          document
+            .querySelector("h1")
+            .textContent = title;
+    }
+}
+
 
