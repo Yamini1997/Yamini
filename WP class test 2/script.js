79af49b57c0c234a1160fe87786b12efced63b91
Yamini1997 <yamini2497@gmail.com>
2017-03-21 15:57:59 +0530

    Update script.js

--- WP class test 2/script.js
+++ WP class test 2/script.js
@@ -1,20 +1,26 @@
-<!DOCTYPE html>
-<html>
-<body>
+function onLoad(){
+	var name = document.getElementById("name").value;
+	// console.log(name);
+	var xhttp = new XMLHttpRequest;
+	xhttp.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status ==200 ) {
+			var jsonObj = JSON.parse(xhttp.responseText);
+			var flag = 1;
+			// console.log(jsonObj.length);
+			for (var i = 0; i < jsonObj.length; i++) {
+				if (jsonObj[i].name == name) {
+					document.getElementById("demo").innerHTML = "Hello "+name+",<br>WP Marks : "+jsonObj[i].wp+"<br>MPMC Marks : "+jsonObj[i].mpmc;
+					break;
+				}else{
+					flag = 0;
+				}
+				if (flag == 0) {
+					document.getElementById("demo").innerHTML = "Incorrect Name";	
+				}
+			}
+		}
+	};
+	xhttp.open("GET","data.json",true);
+	xhttp.send(null);
+}
 
-<p id="demo"></p>
-
-<script>
-var person = new Object();
-person.firstName = "Ed";
-person.lastName = "Sheeran";
-person.course1 = "Math";
-person.course2 = "English";
-person.Marks1 = "65";
-person.Marks2 = "80";
-
-document.getElementById("demo").innerHTML =
-" Name: " + person.firstName + "  " + person.lastName + " Course: " + person.course1 + " Marks: " + person.Marks1  + " Course: " + person.course2 + " Marks: " + person.Marks2;
-</script>
-</body>
-</html>
