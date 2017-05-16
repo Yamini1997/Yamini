dab189fb7c9cefd670f47b0516c047f9192fdb32
Yamini1997 <yamini2497@gmail.com>
2017-05-16 11:35:19 +0530

    Create script.js

---
+++ m4/script.js
@@ -0,0 +1,32 @@
+function loadData()
+	{
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function(){
+		if(this.readyState == 4 && this.status==200){
+		var jsonObj = JSON.parse(this.responseText);
+	responseHandler(jsonObj);
+	}
+};
+	function responseHandler(jsonObj){
+		var i = 0;
+		var number = document.getElementById('numberBox').value;
+		var output = document.getElementById('output');
+		var flag = 0;
+// console.log(number);
+	for (var i = 0; i < jsonObj.length; i++) 
+	{
+		if (number == jsonObj[i].number) 
+		{
+		output.innerHTML = jsonObj[i].name;
+		flag = 1;
+		break;
+	}
+		}
+		if (flag == 0) 
+		{
+		output.innerHTML = "<span style='color:red;'>NOT FOUND</span>";
+		}
+	}
+		xhttp.open("GET","wp.json",true);
+		xhttp.send(); 
+}
